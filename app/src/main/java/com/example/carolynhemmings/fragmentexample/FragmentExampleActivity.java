56d58e62b601ebced928a18e2968076be7e153ff
Carolyn Hemmingsexit <cphemm@msn.com>
2016-05-27 19:03:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/carolynhemmings/fragmentexample/FragmentExampleActivity.java
@@ -0,0 +1,18 @@
+package com.example.carolynhemmings.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class FragmentExampleActivity extends FragmentActivity implements ToolbarFragment.ToolbarListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_example);
+    }
+
+    public void onButtonClick(int fontsize, String text) {
+        TextFragment textFragment = (TextFragment) getSupportFragmentManager().findFragmentById(R.id.text_fragment);
+        textFragment.changeTextProperties(fontsize, text);
+    }
+}
